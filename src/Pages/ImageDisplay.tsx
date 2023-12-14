4adffe6508907fd09e5d3fc825622907358606b9
Erik Landaverde <landaverde.erik10@gmail.com>
2023-12-14 11:24:26 -0800

    Add loading spinner for image display

--- src/Pages/ImageDisplay.tsx
+++ src/Pages/ImageDisplay.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import '../App.css';
+import { BarLoader, HashLoader } from 'react-spinners';
 
 function ImageDisplay() {
     let [images, setImages] = useState<string[]>([]);
+    let [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         fetch("https://melysite.azurewebsites.net/api/MelyPhotos/GetImages")
@@ -12,13 +14,15 @@ function ImageDisplay() {
             data.photos.slice().reverse().forEach((image:any, index:number) => {
                 byteStrings.push(image.photo);
             });
-            setImages(byteStrings);});
-        
+            setImages(byteStrings);
+            setIsLoading(false);
+        });
     }, []);
 
     return (
         <div className="ImageDisplay">
             <div className="flexRow">
+                {isLoading ? <div className="HashLoaderDiv"><HashLoader color="#9436d6" /></div> : ""}
                 {images.map(item =>
                 <div className="flexItemImage">
                     <img src={"data:image/jpg;base64," + item} className="image" alt="portfolio"/>
@@ -29,3 +33,10 @@ function ImageDisplay() {
 }
 
 export default ImageDisplay;
+
+{/* <div className="flexRow">
+                    {images.map(item =>
+                    <div className="flexItemImage">
+                        <img src={"data:image/jpg;base64," + item} className="image" alt="portfolio"/>
+                    </div>)}
+                </div> */}
